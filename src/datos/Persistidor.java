dcb2d0a9b0d5fb00a4ea5de37616a4c4fcbb95e4
godaycn87@outlook.com <godaycn87@outlook.com>
2020-06-28 20:46:54 -0300

    persisto 10 tickets en db
    persisto 10 tickets en db del 1 al 6 de junio

--- src/datos/Persistidor.java
+++ src/datos/Persistidor.java
@@ -1,24 +1,19 @@
 package datos;
 
-import java.net.http.HttpResponse;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-
 import org.bson.Document;
-
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.mongodb.ConnectionString;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.bulk.WriteRequest.Type;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.util.JSON;
 
-import org.bson.Document;
+import modelo.Ticket;
+
+import static com.mongodb.client.model.Filters.*;
 
 public class Persistidor {
 	
@@ -38,9 +33,40 @@ public class Persistidor {
         Document documentoTicket = new Document(map);
 		
         //inserto en coleccion
-		collection.insertOne(documentoTicket);
-				
+		collection.insertOne(documentoTicket);	
+	}
+	
+	public void persistirTickets(List<String> jsonTickets) {
+		MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
+		MongoClient mongoClient = new MongoClient(connectionString);
+		MongoDatabase database = mongoClient.getDatabase("Farma");
 		
+	//PARSEAR
+		//Abro collecion si existe, sino la creo
+		MongoCollection<Document> collection =  database.getCollection("Tickets");		
+     	//convierto el json en map para insertarlo
+	
+		Iterator<String> it = jsonTickets.iterator();
+		
+		while (it.hasNext()) {
+			Gson gson = new Gson();
+			String aInsertar = new String(it.next());
+			
+			Ticket tAInsertar = gson.fromJson(aInsertar, Ticket.class);
+			
+			Map map = gson.fromJson(aInsertar, Map.class);
+	        Document documentoTicket = new Document(map);
+	        
+	        if (collection.find(eq("nroTicket", tAInsertar.getNroTicket())).first() == null) {
+		        //Si no existe inserto en coleccion
+	        	System.out.print("NO existe registro");
+				collection.insertOne(documentoTicket);	        	
+	        }
+	        else {	        //Si no existe informo
+	        	System.out.print("Ya existe registro");
+	        }	     
+		}
+			
 	}
 	
 }
--- src/test/test.java
+++ src/test/test.java
@@ -1,6 +1,9 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import modelo.Cliente;
 import modelo.Domicilio;
@@ -15,27 +18,114 @@ import datos.Persistidor;
 
 public class test {
 	public static void main (String [ ] args) {
-		System.out.print("test");
 		
-		Domicilio d = new Domicilio(1, "calle", 123, "localidad", "Provincia");
-		ObraSocial oS = new ObraSocial(1, "Obra Social");
-		
-		Cliente c = new Cliente(33211333, "Jose", "Perez", d, oS);
-		Empleado eAtendio = new Empleado(1,"Atendio","Lo","222222222",oS,d);
-		Empleado eCobro = new Empleado(2,"Cobro","Le","222222223",oS,d);
-		Producto p = new Producto(111,"producto","descripcion","laboratorio",123);
-		Sucursal s = new Sucursal(1, 123, d, eCobro);
-		FormaDePago f = new FormaDePago(1,"Efectivo");
-		Date fecha = new Date();
-		Ticket t = new Ticket(1, fecha, 200, f, eCobro, eAtendio, c,s);
-		
-		SerializadorTicket serializador = new SerializadorTicket();
-		String jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t);
-			
 		//Persistir datos
 		Persistidor persistidor = new Persistidor();
-		persistidor.persistirTicket(jsonTicket);
-		
+		persistidor.persistirTickets(cargarTicketsGenericos(1));
 		
 	}
+	
+	public static List<String> cargarTicketsGenericos(int cantAGenerar) {
+		
+		//creo 2 obras sociales
+		ObraSocial oS1 = new ObraSocial(1, "OSde");
+		ObraSocial oS2 = new ObraSocial(2, "Accord");
+		
+		//Creo 6 Clientes		
+     	Cliente c1 = new Cliente(1, "Jose", "Perez", new Domicilio(1, "calle", 100, "localidad", "Provincia"), oS1);		
+		Cliente c2 = new Cliente(2, "Juan", "Gomez", new Domicilio(2, "calle", 200, "localidad", "Provincia"), oS2);	
+		Cliente c3 = new Cliente(3, "Jorge", "Gonzalez", new Domicilio(3, "calle", 300, "localidad", "Provincia") , oS1);
+		Cliente c4 = new Cliente(4, "Pedro", "Picapiedras", new Domicilio(4, "calle", 400, "localidad", "Provincia") , oS2);
+		Cliente c5 = new Cliente(5, "Super", "Sonico", new Domicilio(5, "calle", 500, "localidad", "Provincia") , oS1);		
+		Cliente c6 = new Cliente(6, "Stephen", "Strange", new Domicilio(6, "calle", 600, "localidad", "Provincia") , oS2);
+		
+		//Creo 3 empleados
+		Empleado e1 = new Empleado(1,"Bruce","Banner","222222222",oS1, new Domicilio(7, "calle", 700, "localidad", "Provincia") );
+		Empleado e2 = new Empleado(2,"Anthony","Stark","222222222",oS2, new Domicilio(8, "calle", 800, "localidad", "Provincia") );
+		Empleado e3 = new Empleado(3,"Steven","Rogers","222222222",oS1, new Domicilio(9, "calle", 900, "localidad", "Provincia") );
+		Empleado e4 = new Empleado(4,"Hank","Pym","222222222",oS1, new Domicilio(12, "calle", 900, "localidad", "Provincia") );
+		
+		//Creo 5 productos
+		Producto p1 = new Producto(1,"Ibuprofeno","descripcion","laboratorio",100);
+		Producto p2 = new Producto(2,"Paracetamol", "descripcion","laboratorio",50);
+		Producto p3 = new Producto(3,"Perfume","descripcion","laboratorio",1200);
+		Producto p4 = new Producto(4,"Curitas","descripcion","laboratorio",10);
+		Producto p5 = new Producto(51,"Amoxidal","descripcion","laboratorio",75);
+		
+		//creo 2 sucursales
+		Sucursal s1 = new Sucursal(1, 1, new Domicilio(10, "calle", 1000, "localidad", "Provincia") , e1);
+		Sucursal s2 = new Sucursal(1, 2, new Domicilio(11, "calle", 1100, "localidad", "Provincia") , e2);
+		
+		//Creo 3 formas de pago
+		FormaDePago f1 = new FormaDePago(1,"Efectivo");
+		FormaDePago f2 = new FormaDePago(2,"Debito");
+		FormaDePago f3 = new FormaDePago(2,"Credito");
+		
+		Date fecha = new Date();
+		fecha.setDate(1);		
+		//TICKETS DIA 1
+		Ticket t1 = new Ticket(1, fecha, 100, f1, e1, e2, c1,s1);
+		Ticket t2 = new Ticket(2, fecha, 200, f2, e1, e2, c2,s1);
+		Ticket t3 = new Ticket(3, fecha, 150, f3, e3, e4, c3,s2);
+		Ticket t4 = new Ticket(4, fecha, 1200, f1, e3, e4, c4,s2);
+		//TICKETS DIA 2		
+		fecha.setDate(2);
+		Ticket t5 = new Ticket(5, fecha, 100, f1, e1, e2, c5,s1);
+		//TICKETS DIA 4
+		fecha.setDate(4);
+		Ticket t6 = new Ticket(6, fecha, 400, f2, e3, e4, c6,s2);
+		Ticket t7 = new Ticket(7, fecha, 500, f2, e3, e4, c6,s2);
+		Ticket t8 = new Ticket(8, fecha, 200, f3, e3, e4, c6,s2);
+		//TICKETS DIA 6
+		fecha.setDate(6);
+		Ticket t9 = new Ticket(9, fecha, 1200, f3, e1, e2, c2,s1);
+		Ticket t10 = new Ticket(10, fecha, 500, f3, e3, e4, c2,s2);
+		
+		List<String> listaTickets = new ArrayList<String>();
+		
+		SerializadorTicket serializador = new SerializadorTicket();
+		String jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t1);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t2);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t3);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t4);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t5);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t6);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t7);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t8);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t9);
+		listaTickets.add(jsonTicket);
+		
+		serializador = new SerializadorTicket();
+		jsonTicket = serializador.debeDevolverLaRepresentacionJSONDeUnObjeto(t10);
+		listaTickets.add(jsonTicket);
+		
+	
+		return listaTickets;
+		
+	}
+	
 }
